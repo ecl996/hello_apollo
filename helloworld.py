5e92e45ee22dc550b86fa5a7b4e7a5a3661b77c0
ecl996 <92390036+ecl996@users.noreply.github.com>
2021-10-15 17:22:10 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,7 @@ import datetime
 
 @route('/hello')
 def index():
-    return f'<b>Hello {datetime.datetime.now()}</b>!'
+    message = "<b>Hello " + datetime.datetime.now() + "</b>!"
+    return message
 
 run(host='0.0.0.0', port=8080)
